ebc2b34d91c4305bec9f12054c3613bf83ea4147
Lucas <108037570+Kosgladx@users.noreply.github.com>
2026-05-24 23:06:54 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,135 +1,141 @@
-#import psycopg2
-import database
-import random
-x = open('Hackathon - Respostas ao formulário 1.csv','r',encoding='utf-8')
-z = x.read()
-x.close()
-z = z.split('\n')
-for i in range(0,5):
-    z.pop(0)
+import psycopg2
 
-lideres = []# tem email,nome,matrícula,telefone,periodo,matriculalider,senha
-Alunos_Comuns = []# tem email,nome,matrícula,telefone,periodo,matriculalider,senha
-Sem_Grupo = []# tem email,nome,matrícula,telefone,periodo
-Monitores = []
+def connect_to_database():
+    try:
+        connection = psycopg2.connect(
+            host="localhost",
+            database="hackatonteste2",
+            port = "5433",
+            user="postgres",
+            password="z9f4k5ty"
+        )
+        cursor = connection.cursor()
+        print("Database connection successful!")
+        return connection, cursor
+    
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    return
 
-connection, cursor = database.connect_to_database()
-
-for i in z:
-    temp = i.split(',')
-    if len(temp)== 9:# o 1 adicional é a data do registro no temp[0]
-        #print('comum')
-        if temp[6] == "Sim (Sou líder do grupo)":
-            telefone = temp[4]
-            telefone_final = '' 
-            for i in telefone:
-             if i not in '0123456789':
-                 pass
-             else:
-                 telefone_final = telefone_final + i
-                
-            lideres.append({'email':temp[1],'nome':temp[2],'matricula':temp[3],'telefone':temp[4],'periodo':temp[5],'matriculalider':temp[7],'senha':temp[8]})
-        elif temp[6] == "Sim (integrante do grupo)":
-            telefone = temp[4]
-            telefone_final = '' 
-            for i in telefone:
-             if i not in '0123456789':
-                 pass
-             else:
-                 telefone_final = telefone_final + i #alunos comuns
-            Alunos_Comuns.append({'email':temp[1],'nome':temp[2],'matricula':temp[3],'telefone':temp[4],'periodo':temp[5],'matriculalider':temp[7],'senha':temp[8]})
-        else:
-            Sem_Grupo.append({'email':temp[1],'nome':temp[2],'matricula':temp[3],'telefone':temp[4],'periodo':temp[5],'matriculalider':0,'senha':0})
-         
+def close_database_connection(connection, cursor):
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
+        print("Database connection closed.")
+    return
 
 
-    else:
-        pass#print('incomum',len(temp))
-#[[],[],[]]
+
+def execute_query(cursor, query, params=None):
+    try:
+        cursor.execute(query, params)
+        return cursor.fetchall()
+    except (Exception, psycopg2.Error) as error:
+        print("Error executing query", error)
+        return None
+    
 
 
-x = open('Hackathon_Monitor - Respostas ao formulário 1.csv','r',encoding='utf-8')
-z = x.read()
-x.close()
-z = z.split('\n')
-for i in range(0,1):
-    z.pop(0)
+def adicionar_monitor(cursor, connection, telefone, experiencia, nome, email):
+    monitor_exists = execute_query(cursor, "SELECT * FROM monitores WHERE telefone = %s", (telefone))
+    if monitor_exists:
+        print(f"Monitor {nome} já existe, falha no cadastro.")
+        return
+    execute_query(cursor, "INSERT INTO monitores (telefone, experiencia, nome, email) VALUES (%s, %s, %s, %s)", (telefone, experiencia, nome, email))
+    connection.commit()
+    return 
 
-#print(z)
+def adicionar_lider(cursor, connection, telefone, matricula, nome, periodo, grupo, senha, email, monitor):
+    erro=0
+    aluno_exists = execute_query(cursor, "SELECT * FROM alunos WHERE telefone = %s", (telefone))
 
-for i in z:
+    if aluno_exists:
+        print(f"Aluno {nome} já existe, falha no cadastro.")
+        return
+    execute_query(cursor, "INSERT INTO grupos (id, senha, monitor) VALUES (%s, %s, %s)", (grupo, senha, monitor))
+    execute_query(cursor, "INSERT INTO alunos (telefone, matricula, nome, periodo, grupo, email, erro) VALUES (%s, %s, %s, %s, %s, %s, %s)", (telefone, matricula, nome, periodo, grupo, email, erro))
+    connection.commit()
+    return
 
-    temp = i.split(',')
-    temp2 = i.split(',')
-    temp2.pop(0)
-    temp2.pop(0)
-    temp2.pop(0)
-    temp2.pop(0)
-    temp3 = []
-    for i in temp2[0]:
-        if i in '0123456789':
-            temp3.append(int(i))
-    telefone = temp[3]
-    telefone_final = '' 
-    for i in telefone:
-     if i not in '0123456789':
-         pass
-     else:
-         telefone_final = telefone_final + i
-    Monitores.append({'email':temp[1],'nome':temp[2],'telefone':telefone_final,'experiencia':temp3})
+def adicionar_aluno(cursor, connection, telefone, matricula, nome, periodo, grupo, senha, email):
+    erro=0
 
-#print(Monitores,Alunos_Comuns,Sem_Grupo)
-#input()
-for m in Monitores:
-    database.adicionar_monitor(cursor, connection, m['telefone'], m['experiencia'], m['nome'], m['email'])
+    aluno_exists = execute_query(cursor, "SELECT * FROM alunos WHERE telefone = %s", (telefone,))
+    if aluno_exists:
+        print(f"Aluno {nome} já existe, falha no cadastro.")
+        return
+    
+    grupo_exists = execute_query(cursor, "SELECT * FROM grupos WHERE id = %s", (grupo,))
+    print(grupo_exists)
+    if not grupo_exists:
+        print(f"Grupo {grupo} não encontrado.")
+        erro = 1
+        grupo = 0
+    elif grupo_exists[0][1] != senha:
+        print(f"Senha incorreta para o grupo {grupo}.")
+        erro = 2
+        grupo = 0
 
-for a in Alunos_Comuns:
-    database.adicionar_aluno(cursor, connection, a['telefone'], a['matricula'], a['nome'], a['periodo'], a['matriculalider'], a['senha'], a['email'])
+    execute_query(cursor, "INSERT INTO alunos (telefone, matricula, nome, periodo, grupo, email, erro) VALUES (%s, %s, %s, %s, %s, %s, %s)", (telefone, matricula, nome, periodo, grupo, email, erro)) 
+    connection.commit()
+    return
 
-for sg in Sem_Grupo:
-    database.adicionar_aluno(cursor, connection, sg['telefone'], sg['matricula'], sg['nome'], sg['periodo'], sg['matriculalider'], sg['senha'], sg['email'])
+def adicionar_semwhats(connection, cursor, telefone):
+    semwhats_exists = execute_query(cursor, "SELECT * FROM semwhats WHERE id = %s", (telefone,))
+    if semwhats_exists:
+        print(f"Telefone {telefone} já existe na lista de sem WhatsApp, falha no cadastro.")
+        return
+    execute_query(cursor, "INSERT INTO semwhats (id) VALUES (%s)", (telefone,))
+    connection.commit()
+    return
 
-def aleatorizar_lista(lista):
-    lista = lista
-    lista2 = []
-    while len(lista)!=0:
-        if len(lista) > 1:
-            z = random.randint(0,len(lista)-1)
-        else:
-            z = 0 
-        lista2.append(lista[z])
-        lista.pop(z)
-    lista = lista2.copy()
-    return lista2
-        
-        
-for l in lideres:
-    objetivo = l['periodo']
-    atingido = False
-    possibilidade = False
-    Monitores = aleatorizar_lista(Monitores)
-    while atingido == False:
-        for m in Monitores:
-            if objetivo in m:
-                database.adicionar_lider(cursor, connection, l['telefone'], l['matricula'], l['nome'], l['periodo'], l['matriculalider'], l['senha'], l['email'], m['telefone'])
-                possibilidade = True
-                atingido = True
-                break
+def adicionar_grupo(cursor, connection, id, senha, monitor):
+    grupo_exists = execute_query(cursor, "SELECT * FROM grupos WHERE id = %s", (id,))
+    if grupo_exists:
+        print(f"Grupo {id} já existe, falha no cadastro.")
+        return
+    execute_query(cursor, "INSERT INTO grupos (id, senha, monitor) VALUES (%s, %s, %s)", (id, senha, monitor))
+    connection.commit()
+    return
 
-        if possibilidade == False:
-            Monitores = aleatorizar_lista(Monitores)
-            if len(Monitores) > 1:
-                escolhido = Monitores[random.randint(0,len(Monitores)-1)]
-            else:
-                escolhido = Monitores[0]
-            database.adicionar_lider(cursor, connection, l['telefone'], l['matricula'], l['nome'], l['periodo'], l['matriculalider'], l['senha'], l['email'], escolhido['telefone'])
-            possibilidade = True
-            atingido = True
+def adicionar_monitor(cursor, connection, telefone, experiencia, nome, email):
+    monitor_exists = execute_query(cursor, "SELECT * FROM monitores WHERE telefone = %s", (telefone,))
+    if monitor_exists:
+        print(f"Monitor {nome} já existe, falha no cadastro.")
+        return
+    execute_query(cursor, "INSERT INTO monitores (telefone, experiencia, nome, email) VALUES (%s, %s, %s, %s)", (telefone, experiencia, nome, email))
+    connection.commit()
+    return
 
-           
-#listaprint = database.listar_monitores(cursor)
-#print(listaprint)
-#listaprint = database.listar_grupos(cursor)
-#print(listaprint)
-#listaprint = database.listar_alunos(cursor)
-#print(listaprint)
+def listar_monitores(cursor):
+    return execute_query(cursor, "SELECT * FROM monitores")
+
+def listar_alunos(cursor):
+    return execute_query(cursor, "SELECT * FROM alunos")
+
+def listar_grupos(cursor):
+    return execute_query(cursor, "SELECT * FROM grupos")
+
+def listar_alunos_semwhats(cursor):
+    return execute_query(cursor, "SELECT * FROM semwhats")
+
+def listar_alunos_por_grupo(cursor, grupo):
+    return execute_query(cursor, "SELECT * FROM alunos WHERE grupo = %s", (grupo,))
+
+def listar_alunossem_grupo(cursor):
+    return execute_query(cursor, "SELECT * FROM alunos WHERE grupo = 0")
+
+def listar_alunos_com_erro(cursor):
+    return execute_query(cursor, "SELECT * FROM alunos WHERE erro != 0")
+
+def listar_telefones_grupos(cursor):
+    grupos = execute_query(cursor, "SELECT id,monitor FROM grupos WHERE monitor IS NOT NULL AND id != 0")
+    alunos = [[] for _ in range(len(grupos))]
+    for i, grupo in enumerate(grupos):
+        grupo_alunos = execute_query(cursor, "SELECT telefone FROM alunos WHERE grupo = %s", (grupo[0],))
+        alunos[i] = grupo_alunos + [grupo[1]]
+    return alunos
+
+def listar_telefones(cursor):
+    return execute_query(cursor, "SELECT telefone FROM alunos ") + execute_query(cursor, "SELECT telefone FROM monitores ")
